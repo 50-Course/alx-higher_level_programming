77db78c700b1cfe8ae337a6c80f0b843d9f146a3
Eri Adeodu <erioluwaadeodu@my.uopeople.edu>
2022-07-04 01:50:45 +0000

    Print Lists

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def print_list(my_list=[]):
+def print_list_integer(my_list=[]):
     if isinstance(my_list, list) and my_list is not None:
         for i in my_list:
             if isinstance(i, int):
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     Prints the integers of an array seqeuntially,
     if the array is empty return NOne.
     """
-    print_list(my_list=[1, 2, 3, 4, 5])
+    print_list_integer(my_list)
