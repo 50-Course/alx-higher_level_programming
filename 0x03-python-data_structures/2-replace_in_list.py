c8373648aba987e5206224aa4c4bf449ea9c1fcc
Eri Adeodu <erioluwaadeodu@my.uopeople.edu>
2022-07-04 02:39:29 +0000

    hotfix Task #2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
-def print_reversed_list_integer(my_list, idx, new_element):
+def replace_in_list(my_list, idx, new_element):
     """
     Replaces an element of a list at a specific postition. If idx is negative, return None.
     If out of range, such that idx > my_list return None.
     """
     length = len(my_list)
     if idx < 0 or idx > length - 1:
-        return None
+        return my_list
     my_list[idx] = new_element
     return (my_list)
 
--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -4,9 +4,16 @@ print_list_integer = __import__('0-print_list_integer').print_list_integer
 my_list = [1, 2, 3, 4, 5]
 print_list_integer(my_list)
 
-element_at = __import__('1-element_at').element_at
-idx = 3
-print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+# element_at = __import__('1-element_at').element_at
+# idx = 3
+# print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
 
-print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
-print_reversed_list_integer(my_list)
+replace_in_list = __import__('2-replace_in_list').replace_in_list
+idx = 2
+new_element = 15
+new_list = replace_in_list(my_list, idx, new_element)
+print(new_list)
+print(my_list)
+
+# print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
+# print_reversed_list_integer(my_list)
