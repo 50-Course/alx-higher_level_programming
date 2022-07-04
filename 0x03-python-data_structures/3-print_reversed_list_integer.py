4e9fa37cdb14aae4a6f292b6e29b4e2e36eadd1d
Eri Adeodu <erioluwaadeodu@my.uopeople.edu>
2022-07-04 08:39:45 +0000

    Task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-
 def print_reversed_list_integer(my_list=[]):
-    """
-    Prints all integers of a list in reverse order,
-    one line at a time.
-    """
+
     if not my_list:
-       pass
+        pass
     else:
         my_list.reverse()
         for i in range(len(my_list)):
