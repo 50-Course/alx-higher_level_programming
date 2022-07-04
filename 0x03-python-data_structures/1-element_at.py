fbbd5786f4355aa9f9a323a3bf808e2eb8f5618d
Eri Adeodu <erioluwaadeodu@my.uopeople.edu>
2022-07-04 02:14:03 +0000

    hotfix #5

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -6,6 +6,6 @@ def element_at(my_list, idx):
     If out of range, such that idx > my_list return None.
     """
     
-    if idx < 0 or (idx > len(my_list) - 1):
+    if idx < 0 or idx > (len(my_list) - 1):
         return None
     return my_list[idx]
