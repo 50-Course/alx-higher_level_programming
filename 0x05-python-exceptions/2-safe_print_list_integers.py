6e3dc6b9061e79c7333a3ec7916f2befe13b4e77
50-Course <erioluwaadeodu@my.uopeople.edu>
2022-07-18 18:30:53 -0700

    :recycle: refactor: refactored bit part to match failing test.

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,8 +5,8 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
-        except (Exception, ValueError, TypeError):
-            raise
+        except (ValueError, TypeError):
+            pass
         else:
             count += 1
 
