573bcdf72af4bf96cf09c7bfafd9cdc2cecb5855
50-Course <erioluwaadeodu@my.uopeople.edu>
2022-07-18 17:32:37 -0700

    [chore]: Function that prints X elements in a list.

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,11 @@
 #!/bin/python3
-import typing
 
-def safe_print_integer(value: typing.Any) -> bool:
-   try:
-       if isinstance(value, int):
-           return True
-       else:
-           return False
-   except ValueError:
-       raise
+
+def safe_print_integer(value):
+    try:
+        if isinstance(value, int):
+            return True
+        else:
+            return False
+    except ValueError:
+        raise
