5539e7bcf59d5a9c274090399135d399bcbb5b0b
50-Course <erioluwaadeodu@my.uopeople.edu>
2022-07-23 06:14:20 -0700

    :sparkles: New Exercise Unit: TDD in Python.

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,11 +4,10 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-        print("{:d}".format(value))
+        print("{:d}".format(value), end="\n")
     except Exception as inst:
         sys.stderr.write("Exception: {}".format(inst))
         return False
     else:
         return True
 
-    print()
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,5 @@
+# 7: :test_tube: Test Driven Development in Python
+Test Driven Development in Python for software reliablity and scalability.
+
+
+
