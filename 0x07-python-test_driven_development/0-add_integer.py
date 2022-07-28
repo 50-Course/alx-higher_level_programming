6cdfede6ba8ee8d9e4e8780628122c438940f0d9
50-Course <erioluwaadeodu@my.uopeople.edu>
2022-07-27 18:23:06 -0700

    :test_tube: test: Added test for add function.

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,17 +5,18 @@ Returns the addition of two integers.
 Author: @50-Course (GitHub)
 """
 
-def add_integer(a: int, b: int=98) -> int:
+
+def add_integer(a: int, b: int = 98) -> int:
     """
     Returns the result of two inputs, if both
     are either float or integer.
-    
+
     Raises:
         TypeError
     """
     if not type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    
+
     if not type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
 
