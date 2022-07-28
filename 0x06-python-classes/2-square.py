bacdd0444fed074332f06ffbadad1da5f5a7aed0
50-Course <erioluwaadeodu@my.uopeople.edu>
2022-07-28 15:03:56 -0700

    :sparkles: created a square that raises exception validation when wrongly called.

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+Defines a square class.
+
+Usage:
+    Instantiate the module in your script.
+
+    ```
+    from 0-square import Square
+
+    square = Square()
+    ```
+"""
+
+
+class Square:
+    """
+    Returns a python object.
+    """
+
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
