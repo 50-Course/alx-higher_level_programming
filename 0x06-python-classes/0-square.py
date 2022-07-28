a4896e7218c6bca753b1c5207fbbeea84ed6d6da
50-Course <erioluwaadeodu@my.uopeople.edu>
2022-07-28 14:38:35 -0700

    :sparkles: created a class that defines a Square object.

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+Defines a square class.
+
+Usage:
+    Instantiate the module in your script. 
+    
+    ```
+    from 0-square import Square
+
+    square = Square()
+    ```
+"""
+
+
+class Square:
+    """
+    Returns a python object.
+    """
+    pass
