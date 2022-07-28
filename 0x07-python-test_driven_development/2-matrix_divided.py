06ad350d813d8c0a3900f25e12b376fb19a3feb8
50-Course <erioluwaadeodu@my.uopeople.edu>
2022-07-28 13:33:55 -0700

    :tada: working with Python Classes & OOP

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,7 +5,7 @@ This module is composed by a function that divides the numbers of a matrix
 
 
 def matrix_divided(matrix, div):
-    """ Function that divides the integer/float numbers of a matrix
+    """Function that divides the integer/float numbers of a matrix
     Args:
         matrix: list of a lists of integers/floats
         div: number which divides the matrix
@@ -47,4 +47,4 @@ def matrix_divided(matrix, div):
         len_e = len(elems)
 
     m = list(map(lambda x: list(map(lambda y: round(y / div, 2), x)), matrix))
-    return (m)
+    return m
