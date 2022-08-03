bf2d0d737a61a25b1b0e413bb7cc0efc0409ac8c
50-Course <erioluwaadeodu@my.uopeople.edu>
2022-08-03 01:02:41 -0700

    :sparkles: Created a function that prints a text file to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+File Reader module.
+
+Usage:
+    ```
+    from 0-read_file import read_file
+    
+    file_in = read_file(file_name="")
+    ```
+"""
+
+
+def read_file(file_name: str =""):
+    """
+    Takes a file argument and prints the
+    the result to stdout.
+
+    """
+    if file_name is not None:
+        try:
+            with open(file_name, 'r+') as f:
+                values = f.readlines()
+        except (FileNotFoundError):
+            pass
+        
+        print(values)
