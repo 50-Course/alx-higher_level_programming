8789025d9f3bc6e53e009ab98ac12bba04f78021
50-Course <erioluwaadeodu@my.uopeople.edu>
2022-08-03 05:06:30 -0700

    :sparkles: feat: Task 8-10

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,8 +7,8 @@ functions:
 
 
 def read_file(filename=""):
-    """ reads a text file (UTF8) and prints it to stdout """
+    """reads a text file (UTF8) and prints it to stdout"""
 
-    with open(filename, 'r', encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         read_data = f.read()
-        print(read_data, end='')
+        print(read_data, end="")
