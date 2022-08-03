cdba245bf47192ee693923abad74e3de4df4c2e5
50-Course <erioluwaadeodu@my.uopeople.edu>
2022-08-03 03:23:36 -0700

    :recycle: refac: refactored script

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -5,11 +5,16 @@ Returns JSON representation of a string
 Functions:
     - to_json_string
 """
+import json
 
 
 def to_json_string(my_obj):
-    if my_obj is not None:
-        import json
+    """
+    Converts python object to a JSON representation
 
+    Returns:
+        JSON
+    """
+    if my_obj:
         text = json.dumps(my_obj)
     return text
