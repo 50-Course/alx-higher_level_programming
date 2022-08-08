844dbe5a832a0435d0754c3eb925275e087e9d45
50-Course <erioluwaadeodu@my.uopeople.edu>
2022-08-08 04:02:49 -0700

    :recycle: fixed failing test

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-Appends a string to the end of a text file,
-and returns the number of characters added.
+File: 4-append_write.py
+Functions:
+ - append_write
 """
 
 
@@ -13,4 +14,4 @@ def append_write(filename="", text=""):
     """
 
     with open(filename, "a", encoding="utf-8") as file:
-        return f.write(text)
+        return file.write(text)
