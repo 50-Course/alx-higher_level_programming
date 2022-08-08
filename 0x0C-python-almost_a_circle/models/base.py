ca61bd3bcd753ade5fdcdb9c737be35859567b8c
50-Course <erioluwaadeodu@my.uopeople.edu>
2022-08-08 08:57:28 -0700

    :construction WIP: Creating a Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,4 +5,18 @@ Base class for every other classes.
 
 
 class Base:
-    pass
+    """ Root class for sub-classes """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Default Constructor. 
+        Takes Optional argument id 
+        """
+
+        if id:
+            self.id = id
+        else:
+            incr = _Base.__nb_objects += 1
+            self.id = incr
