1cd06469e9578e115bbfb3c53bb3b43932d1853b
50-Course <erioluwaadeodu@my.uopeople.edu>
2022-12-20 05:59:58 -0800

    :construction:

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,11 +1,10 @@
--- Creates a database htbn_0d_2 and grants user user_0d_2 SELECT access
-CREATE DATABASE 
-    IF NOT EXISTS 'hbtn_0d_2'; -- create database
-
+-- Creates the database hbtn_0d_2 and the user user_0d_2
+-- The user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
+CREATE DATABASE
+    IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER
     IF NOT EXISTS 'user_0d_2'@'localhost'
-    IDENTIFIED BY 'user_0d_2_pwd'; -- creates the user
-
+    IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
-    ON 'hbtn_0d_2'.*
-    TO 'user_0d_2'@'localhost'; -- Gives READ access to user
+   ON `hbtn_0d_2`.*
+   TO 'user_0d_2'@'localhost';
