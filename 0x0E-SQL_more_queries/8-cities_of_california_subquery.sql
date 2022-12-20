e05b1e0e40fd92bc72182728f89339167f4c4700
50-Course <erioluwaadeodu@my.uopeople.edu>
2022-12-20 07:02:44 -0800

    Fixed bug

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,11 @@
 -- list all the cities of California 
 -- that can be found in the database hbtn_0d_usa
-SELECT *
-    FROM cities, states
-    WHERE states.name = 'California'
-    ORDER BY cities.id ASC;
+-- Results are ordered in ascending cities.id
+SELECT id, name
+    FROM cities
+    WHERE state_id IN
+        (SELECT id
+            FROM states
+            WHERE name = "California"
+    ORDER BY id ASC;
 
