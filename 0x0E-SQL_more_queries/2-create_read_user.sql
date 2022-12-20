f40cdf5730c24c12760c9068a1898573b5177058
50-Course <erioluwaadeodu@my.uopeople.edu>
2022-12-20 05:53:22 -0800

    gives user read access to a database

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,8 +3,9 @@ CREATE DATABASE
     IF NOT EXISTS 'hbtn_0d_2'; -- create database
 
 CREATE USER
-    IF NOT EXISTS 'user_0d_2'@'localhost'; -- creates the user
+    IF NOT EXISTS 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd'; -- creates the user
 
 GRANT SELECT
-    ON 'hbtn_0d_2'
-    TO 'user_0d_2'@'localhost';
+    ON 'hbtn_0d_2'.*
+    TO 'user_0d_2'@'localhost'; -- Gives READ access to user
