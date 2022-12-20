3d53e1e98bbc09b0a46f5db3772bb0ae08d07101
50-Course <erioluwaadeodu@my.uopeople.edu>
2022-12-20 06:30:00 -0800

    Fixed a :bug:

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,10 +1,8 @@
 -- creates a atabase with usa states
 CREATE DATABASE
     IF NOT EXISTS hbtn_0d_usa; -- create the database
-
 CREATE TABLE
     IF NOT EXISTS states (
         id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
         name VARCHAR(256) NOT NULL
         ); -- Create a table
- 
