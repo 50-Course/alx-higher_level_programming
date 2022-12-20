79ec6e53e90842055551599faec06bdb1651cad0
50-Course <erioluwaadeodu@my.uopeople.edu>
2022-12-20 06:46:57 -0800

    Update `cities.sql`

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,10 +3,11 @@ CREATE DATABASE
     IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE
     IF NOT EXISTS hbtn_0d_usa.cities (
-    id INT   AUTO_INCREMENT     NOT NULL,
+    id INT UNIQUE  AUTO_INCREMENT     NOT NULL,
     state_id INT                NOT NULL,
     name     VARCHAR(256)       NOT NULL,
     PRIMARY KEY (id)
     FOREIGN KEY (state_id)
-    REFERENCES states(id)
+    REFERENCES states (id)
     ); -- Creates a cities table
+
