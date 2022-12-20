f4f9708c137968f44b5021f0117c657681f98463
50-Course <erioluwaadeodu@my.uopeople.edu>
2022-12-20 05:49:51 -0800

    grants MySQL user READ access

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+-- Creates a database htbn_0d_2 and grants user user_0d_2 SELECT access
+CREATE DATABASE 
+    IF NOT EXISTS 'hbtn_0d_2'; -- create database
+
+CREATE USER
+    IF NOT EXISTS 'user_0d_2'@'localhost'; -- creates the user
+
+GRANT SELECT
+    ON 'hbtn_0d_2'
+    TO 'user_0d_2'@'localhost';
